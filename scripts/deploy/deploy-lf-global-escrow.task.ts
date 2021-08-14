c35acc05a9cc9adf7aec51a02668d50800e14c74
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-08-14 11:22:58 +0800

    hardhat deploy script for GlobalEscrow

---
+++ scripts/deploy/deploy-lf-global-escrow.task.ts
@@ -0,0 +1,15 @@
+import { task, types } from "hardhat/config";
+import { Logger } from "tslog";
+import "@nomiclabs/hardhat-ethers";
+const log: Logger = new Logger();
+import { strict as assert } from 'assert';
+
+task("deploy-lf-global-escrow", "Deploys a new lfGlobalEscrow contract")
+  .setAction(async (args, hre) => {
+
+  log.info(`deploying lfGlobalEscrow on network: ${hre.network.name}`);
+  const lfGlobalEscrowContract = await hre.ethers.getContractFactory(`contracts/LFGlobalEscrow.sol:LFGlobalEscrow`);
+  const lfGlobalEscrowContractInstance = await lfGlobalEscrowContract.deploy();
+  await lfGlobalEscrowContractInstance.deployed();
+  log.info(`copy ${lfGlobalEscrowContractInstance.address} to constant lfGlobalEscrowAddress of scripts/deploy/config/deploy-config-global.ts`);
+});
