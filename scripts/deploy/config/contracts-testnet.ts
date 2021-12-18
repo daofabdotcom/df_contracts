d3fe2d37d1bea6be420133709c89913a779360a7
kanth <kanth.explorer@gmail.com>
2021-12-18 13:32:57 +0800

    Deployed to RInkeby and verified contract address is updated in contract-testnet.ts

--- scripts/deploy/config/contracts-testnet.ts
+++ scripts/deploy/config/contracts-testnet.ts
@@ -24,7 +24,7 @@ const contracts = {
         chainId: 4,
         isTestnet: true,
         deployerAddress: "0x2Bbe50113D8114EECf33bc4aA8CBF02CA1ef1f5A",
-        dfGlobalEscrowAddress: "0xDD3e4f5F71330954EC14B64D8F2eBE74267602f1"
+        dfGlobalEscrowAddress: "0x866EEfB088f2B05a1CFc9282932882dC98fd29A8"
     }
 };
 
--- scripts/deploy/config/deploy-config-global.ts
+++ scripts/deploy/config/deploy-config-global.ts
@@ -1,2 +1,2 @@
 export const network = "rinkeby";
-export const escrow = "0xDD3e4f5F71330954EC14B64D8F2eBE74267602f1"
+export const escrow = "0x866EEfB088f2B05a1CFc9282932882dC98fd29A8"
